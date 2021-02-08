24a34ddc2e78caba32c89eb83c6a803f475e7096
unknown <jfearnow79@gmail.com>
2021-02-08 16:24:47 -0500

    I have made classes for this game.

---
+++ Robots_vs_Dinosaurs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots_vs_Dinosaurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Robots_vs_Dinosaurs/Robot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots_vs_Dinosaurs
+{
+    public class Robot
+    {
+        public void Name()
+        {
+            string name = "Choose your name: ";
+            Console.WriteLine(name);
+        }
+        
+    }
+}
